e7e7f478d29f12cc0b237682b3fc66319e85688c
jack <jack@jackdeMacBook-Pro.local>
2019-08-14 08:24:42 +0800

    perf: set static directory

--- server/router/static.js
+++ server/router/static.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router')
 const send = require('koa-send') // 静态资源处理中间件
+const path = require('path')
 
 // 只会处理 /dist 开头的静态资源
 const router = new Router({
@@ -7,7 +8,9 @@ const router = new Router({
 })
 
 router.get('*', async (ctx) => {
-  await send(ctx, ctx.path)
+  await send(ctx, ctx.path, {
+    root: path.join(__dirname, '../../dist')
+  })
 })
 
 module.exports = router
